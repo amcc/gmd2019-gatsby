c59b4ccfc29146b2f02430ca26c5dcea67cea0d8
amcc <alistairmcclymont@gmail.com>
2019-06-17 22:11:47 +0100

    info page updated

--- src/components/studentList.js
+++ src/components/studentList.js
@@ -12,40 +12,46 @@ import {
 
 import { rhythm } from "../utils/typography";
 
+const GridBoxCategories = css`
+  overflow: hidden;
+  margin: 0 auto;
+  max-width: 960px;
+  /* padding: 0; */
+  padding: 0 ${rhythm(1)};
+
+  @media (min-width: 40em) {
+    padding: 0 ${rhythm(1 / 2)};
+  }
+  border: 0px solid #ececec;
+`;
 
 const StudentList = ({ students }) => {
   return (
     <Flex
-            // mx={[0, -1, -2]}
-            flexWrap="wrap"
-            css={css`
-              margin-top: ${rhythm(2)};
-              position: relative;
-              z-index: 2;
-            `}
-            mx={[-1, -1, -2]}
-          >
-            <Box
-              width={1}
-              px={[1, 1, 2]}
-              key={`box-recent-projects-students`}
-            />
-
-            {students.edges.map(({ node }) => (
-              <Box
-                width={[1 / 2, 1 / 3, 1 / 5]}
-                px={[1, 1, 2]}
-                key={`box-${node.slug}`}
-                css={[GridBoxContainer, PaddedMobile]}
-              >
-                <div key={node.slug}>
-                  <Link to={`/${node.slug}`} css={{ textDecoration: `none` }}>
-                    <h3>{node.title}</h3>
-                  </Link>
-                </div>
-              </Box>
-            ))}
-          </Flex>
+      // mx={[0, -1, -2]}
+      flexWrap="wrap"
+      css={css`
+        margin-top: ${rhythm(2)};
+        position: relative;
+        z-index: 2;
+        width: 100%;
+      `}
+      mx={[-1, -1, -2]}
+    >
+      {students.edges.map(({ node }) => (
+        <Box
+          width={[1 / 2, 1 / 3, 1 / 4, 1 / 5]}
+          px={[1, 1, 2]}
+          key={`box-${node.slug}`}
+        >
+          <div key={node.slug} css={GridBoxCategories}>
+            <Link to={`/${node.slug}`} css={{ textDecoration: `none` }}>
+              <h3>{node.title}</h3>
+            </Link>
+          </div>
+        </Box>
+      ))}
+    </Flex>
   );
 };
 
--- src/pages/info.js
+++ src/pages/info.js
@@ -7,9 +7,7 @@ import { css } from "@emotion/core"; // https://github.com/gatsbyjs/gatsby/blob/
 // import styled from "@emotion/styled";
 import { Flex, Box } from "@rebass/grid/emotion"; //https://github.com/rebassjs/grid
 
-import {
-  PaddedMobile
-} from "../utils/styles";
+import { PaddedMobile } from "../utils/styles";
 
 class Info extends Component {
   render() {
@@ -18,16 +16,56 @@ class Info extends Component {
     return (
       <Layout>
         <Flex flexWrap="wrap">
-          <Box width={[1, 1, 1 / 2]} css={PaddedMobile}>
-            <h1>Graphic Media Design</h1>
-            <p>
-              London College of Communication Elephant & Castle London SE1 6SB
-              Directions → Private View Wednesday 19 June 6 – 9pm General Access
-              Thursday 20 – Saturday 22 June Thur - Fri: 11am – 7pm Sat: 11am –
-              4pm
-            </p>
+          <Box py={4} width={1}>
+            <section css={PaddedMobile}>
+              <h1>Graphic Media Design</h1>
+            </section>
+          </Box>
+          <Box py={2} width={[1, 1 / 2, 1 / 2]}>
+            <section css={PaddedMobile}>
+              <p>
+                Final year work for Graphic Media Design students graduating in
+                2018
+              </p>
+              <h3>Private View Wednesday 19 June 6 – 9pm</h3>
+              <p>General Access Thursday 20 – Saturday 22 June</p>
+              <p>Thur - Fri: 11am – 7pm Sat: 11am – 4pm</p>
+            </section>
+          </Box>
+          <Box py={2} width={[1, 1 / 2, 1 / 2]}>
+            <section css={PaddedMobile}>
+              <h3>Venue</h3>
+              <p>London College of Communication</p>
+              <p>
+                Elephant & Castle
+                <br />
+                London SE1 6SB
+              </p>
+              <iframe
+                src="https://www.google.com/maps/d/u/0/embed?mid=1vWhIKMxud9J6jTtPCry4-04518y6Ie2e"
+                width="100%"
+                height="600"
+              />
+            </section>
           </Box>
         </Flex>
+        {/* <Flex flexWrap="wrap">
+          <Box py={4} width={1}>
+            <section css={PaddedMobile}>
+              <h2>Credits</h2>
+            </section>
+          </Box>
+          <Box py={2} width={[1, 1 / 2, 1 / 2]}>
+            <section css={PaddedMobile}>
+              
+            </section>
+          </Box>
+          <Box py={2} width={[1, 1 / 2, 1 / 2]}>
+            <section css={PaddedMobile}>
+              
+            </section>
+          </Box>
+        </Flex> */}
 
         {/* <p><a href="http://2018.gmdlcc.com">2018</a></p>
       <p><a href="http://2017.gmdlcc.com">2017</a></p>
