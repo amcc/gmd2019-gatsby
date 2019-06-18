6c0ea1ed291b3db78071f4d350931c9b0a71ef2a
amcc <alistairmcclymont@gmail.com>
2019-06-18 21:33:59 +0100

    added graphql import

--- src/components/studentGrid.js
+++ src/components/studentGrid.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "gatsby";
+import { Link, graphql } from "gatsby";
 import Img from "gatsby-image";
 import { Flex, Box } from "@rebass/grid/emotion"; //https://github.com/rebassjs/grid
 import { css } from "@emotion/core";
