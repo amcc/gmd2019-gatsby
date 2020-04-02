40ab53f6e2e241c24c2753b166701deec40b701f
amcc <alistairmcclymont@gmail.com>
2020-04-02 15:45:21 +0100

    changing source, update included categories

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,7 +63,7 @@ module.exports = {
           "**/posts",
           // "**/student",
           // "**/competency",
-          // "**/categories",
+          "**/categories",
           "**/tags",
           "**/media"
           // "**/taxonomies",
